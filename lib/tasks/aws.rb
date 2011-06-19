3513a6ef4e38afe6c84635df719720e388735663
Andy Marks <andy@corvine.org>
2011-06-19 22:11:08 +1000

    Removed (now) unused tasks

--- lib/tasks/aws.rb
+++ lib/tasks/aws.rb
@@ -3,87 +3,38 @@ require "json"
 require "fog"
 require "fog/aws/cloud_formation"
 
-namespace :aws do
+namespace :drupal do
   AWS_DIR = "#{File.dirname(__FILE__)}/aws"
-  BOOTSTRAP_FILE = "bootstrap.tar.gz"
-  STACK_NAME = "drupal-stack"
 
   directory BUILD_DIR
   
   desc "creates the project's infrastructure in the Amazon cloud"
-  task :provision => :upload_bootstrap_files do
-    template_body = contents("#{AWS_DIR}/cloud_formation_template.erb")
-    boot_script = ERB.new(contents("#{AWS_DIR}/bootstrap.erb")).result(binding)
+  task :provision => :settings do
+    template_body = contents("#{AWS_DIR}/drupal-stack-template.erb")
 
     puts "creating aws stack, this might take a while... ".white
     cloud = cloud_formation
-    cloud.create_stack(STACK_NAME,
+    cloud.create_stack(SETTINGS["cloudformation_stack_name"],
                        "TemplateBody" => ERB.new(template_body).result(binding),
-                       "Parameters" => { "KeyName" => SETTINGS["aws_ssh_key_name"],
-                                         "Password" => "hotbubbles",
-                                         "PSK" => "W3lcom3%1"})
+                       "Parameters" => { "DrupalSiteAdmin" => "admin",
+                                         "DrupalSitePwd" => "admin"})
     stack = nil
     until stack
-      sleep 1
+      sleep 30
+      puts "looking for created stack at #{Time.now}...".yellow
       stack = find_stack(cloud)
     end
     puts "your servers have been provisioned successfully".white
   end
 
-  {"admin_server" => "ManagementConsole", "test_server" => "TestServer"}.each do |file_name, key_name|
-    file "#{BUILD_DIR}/#{file_name}" => ["aws:settings", BUILD_DIR] do
-      cloud = cloud_formation
-      stack = find_stack(cloud)
-      fail "could not find stack... did you provision your aws resources?".red if stack.nil?
-      puts "discovering #{file_name} address...".white
-      address = stack["Outputs"].find { |output| output["OutputKey"] == key_name }["OutputValue"]
-      puts "#{file_name} address: #{address}".green
-      File.open("#{BUILD_DIR}/#{file_name}", "w") do |file|
-        file.write address
-      end
-    end
-  end
-
   desc "stops all instances and releases all Amazon resources"
   task :shutdown => :settings do
-    cloud_formation.delete_stack STACK_NAME
+    cloud_formation.delete_stack SETTINGS["cloudformation_stack_name"]
     puts "shutdown command successful".green
-  end
-
-  task :upload_bootstrap_files => [:settings, :package] do
-    storage = s3
-    directory = nil
-    if not storage.directories.get(BUCKET_NAME)
-      puts "creating S3 bucket".cyan
-      directory = storage.directories.create(:key => BUCKET_NAME, :public => true, :location => "ap-southeast-1")
-    else
-      directory = storage.directories.get(BUCKET_NAME)
-    end
-    puts "uploading bootstrap package...".cyan
-    directory.files.create(:key => BOOTSTRAP_FILE,
-                           :body => File.open("#{BUILD_DIR}/#{BOOTSTRAP_FILE}"),
-                           :public => true,
-                           :location => "ap-southeast-1")
-  end
-
-  task :package => [:clean, BUILD_DIR] do
-    mkdir_p "#{BUILD_DIR}/package"
-    cp_r "#{AWS_DIR}/repos", "#{BUILD_DIR}/package/repos"
-    cp_r "puppet", "#{BUILD_DIR}/package/puppet"
-    puts "packaging boostrap files in #{BOOTSTRAP_FILE}"
-    %x[cd #{BUILD_DIR}/package; tar -zcf ../#{BOOTSTRAP_FILE} *]
   end
 
   task :settings do
     SETTINGS = YAML::parse(open("conf/settings.yaml")).transform
-    BUCKET_NAME = "#{SETTINGS['aws_s3_bucket_name']}"
-  end
-
-  def s3
-    Fog::Storage.new(:provider => "AWS",
-                     :aws_access_key_id => SETTINGS["aws_access_key"],
-                     :aws_secret_access_key => SETTINGS["aws_secret_access_key"],
-                     :region => "ap-southeast-1")
   end
 
   def cloud_formation
@@ -94,7 +45,7 @@ namespace :aws do
 
   def find_stack(cloud)
     cloud.describe_stacks.body["Stacks"].find do |stack|
-      stack["StackName"] == STACK_NAME && stack["StackStatus"] == "CREATE_COMPLETE"
+      stack["StackName"] == SETTINGS["cloudformation_stack_name"] && stack["StackStatus"] == "CREATE_COMPLETE"
     end
   end
 
